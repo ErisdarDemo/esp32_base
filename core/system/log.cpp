56a63f08fbe34a8e079f2aa3f6d0b6a1de8b5feb
Justin Reina <justinmreina@gmail.com>
2025-04-21 19:28:26 -0700

    (C)    log updates

--- core/system/log.cpp
+++ core/system/log.cpp
@@ -5,6 +5,7 @@
  *
  *  @section    Opens
  *      convert log naming lookup to map
+ *		validate routine
  */
 /**************************************************************************************************/
 using namespace std;
@@ -26,6 +27,26 @@ using namespace std;
 //************************************************************************************************//
 
 //-----------------------------------------  Definitions -----------------------------------------//
+
+//Identifiers
+#define LOG_EVENT_A_TYPE_ID 	"Event-A"
+#define LOG_EVENT_B_TYPE_ID 	"Event-B"
+#define LOG_EVENT_C_TYPE_ID 	"Event-C"
+#define LOG_SYSTEM_TYPE_ID 		"System"
+#define LOG_RTOS_TYPE_ID 		"Rtos"
+#define LOG_APP_TYPE_ID 		"App"
+#define LOG_UTIL_TYPE_ID 		"Util"
+#define LOG_GENERAL_TYPE_ID 	"General"
+#define LOG_UNSPEC_TYPE_ID 		"Unspecified"
+#define LOG_UNKNOWN_TYPE_ID		"Unknown"
+
+
+//************************************************************************************************//
+//                                       FUNCTION DECLARATIONS                                    //
+//************************************************************************************************//
+
+//Lookups
+static string Log::log_getIdentifier(Log_message_type msg_type);
 
 
 //************************************************************************************************//
@@ -69,7 +90,7 @@ void Log::init(void) {
 
 
 /**************************************************************************************************/
-/** @fcn        write(Log_message_type src, string msg)
+/** @fcn        void write(Log_message_type src, string msg)
  *  @brief      x
  *  @details    x
  *
@@ -93,42 +114,55 @@ void Log::write(Log_message_type src, string msg) {
     //Locals
     string src_id;                                  /* identified description string for src      */
 
-    //Lookup Id
-    switch(src) {
-        case LOG_TYPE_A:
-            src_id = "Event-A";
-            break;
-        case LOG_TYPE_B:
-            src_id = "Event-B";
-            break;
-        case LOG_TYPE_C:
-            src_id = "Event-C";
-            break;
-        case LOG_TYPE_SYSTEM:
-            src_id = "System";
-            break;
-        case LOG_TYPE_RTOS:
-            src_id = "Rtos";
-            break;
-        case LOG_TYPE_APP:
-            src_id = "App";
-            break;
-        case LOG_TYPE_UTIL:
-            src_id = "Util";
-            break;
-        case LOG_TYPE_GENERAL:
-            src_id = "General";
-            break;
-        case LOG_TYPE_UNSPECIFIED:
-            src_id = "Unspecified";
-            break;
-        default:
-            src_id = "Unknown";
-    }
+
+	//Retrieve
+	src_id = Log::log_getIdentifier(src);
 
     //Record
     printf("%s: %s", src_id.c_str(), msg.c_str());
 
     return;
+}
+
+
+//************************************************************************************************//
+//                                         PRIVATE FUNCTIONS                                      //
+//************************************************************************************************//
+
+/**************************************************************************************************/
+/** @fcn        static string log_getIdentifier(Log_message_type msg_type)
+ *  @brief      lookup for log identifier value
+ *  @details    x
+ *
+ *  @param    [in]  (Log_message_type) src - source of log message
+ *
+ *  @return   (string) identifier value
+ */
+/**************************************************************************************************/
+static string Log::log_getIdentifier(Log_message_type msg_type) {
+
+    //Retrieve Id
+    switch(msg_type) {
+        case LOG_TYPE_A:
+            return LOG_EVENT_A_TYPE_ID;
+        case LOG_TYPE_B:
+            return LOG_EVENT_B_TYPE_ID;
+        case LOG_TYPE_C:
+            return LOG_EVENT_C_TYPE_ID;
+        case LOG_TYPE_SYSTEM:
+            return LOG_SYSTEM_TYPE_ID;
+        case LOG_TYPE_RTOS:
+            return LOG_RTOS_TYPE_ID;
+        case LOG_TYPE_APP:
+            return LOG_APP_TYPE_ID;
+        case LOG_TYPE_UTIL:
+            return LOG_UTIL_TYPE_ID;
+        case LOG_TYPE_GENERAL:
+            return LOG_GENERAL_TYPE_ID;
+        case LOG_TYPE_UNSPECIFIED:
+            return LOG_UNSPEC_TYPE_ID;
+        default:
+            return LOG_UNKNOWN_TYPE_ID;
+    }
 }
 
